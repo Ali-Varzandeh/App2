3b6a4302497967e69eff9aa66fbf366cae1c2cd9
Ali-Varzandeh <alivarzandeh2015@yahoo.co.uk>
2020-12-15 14:55:28 +0000

    finish the plan

--- sql/seeds.sql
+++ sql/seeds.sql
@@ -1,46 +1,46 @@
 
-INSERT INTO CATEGORIES ( CATEGORY_TITLE, CATEGORY_SUB, CATEGORY_DESCRIPTION)
+INSERT INTO CATEGORIES (CATEGORY_TITLE, CATEGORY_SUB, CATEGORY_DESCRIPTION)
 VALUES ('Mens CLOTHING ', 'Coats & Jackets' , 'CANADA GOOSE Coat, Exterior pockets, Hooded neck Zip fastening with button placket Regular fit
 Dry clean, Made in Canada');
 
--- INSERT INTO CATEGORIES ( CATEGORY_TITLE, CATEGORY_DESCRIPTION)
--- VALUES (' Womens CLOTHING', 'Midi Dress, Mushrooms, botanicals and polka dots, Autumn Print Pussybow Midi Dress, 100% Silk ');
+INSERT INTO CATEGORIES (CATEGORY_TITLE, CATEGORY_SUB, CATEGORY_DESCRIPTION)
+VALUES (' Womens CLOTHING', 'dresses','Midi Dress, Mushrooms, botanicals and polka dots, Autumn Print Pussybow Midi Dress, 100% Silk ');
 
--- INSERT INTO CATEGORIES ( CATEGORY_TITLE, CATEGORY_DESCRIPTION)
--- VALUES ('Distressed Logo T-Shirt', 'Match the vintage-like aesthetic with his favourite distressed denim, Distressed Logo T-Shirt, 100% Cotton');
+INSERT INTO CATEGORIES (CATEGORY_TITLE, CATEGORY_SUB, CATEGORY_DESCRIPTION)
+VALUES ('Kids & Baby','Boys T-Shirt', 'Distressed Logo T-Shirt , Match the vintage-like aesthetic with his favourite distressed denim, Distressed Logo T-Shirt, 100% Cotton');
 
--- INSERT INTO CATEGORIES ( CATEGORY_TITLE, CATEGORY_DESCRIPTION)
--- VALUES ('BABY GIRLS ROMPERS', 'aymarket check romper from BURBERRY KIDS featuring Haymarket check pattern, band collar, long sleeves, 
--- front button placket and concealed press-stud fastening, 100% Cotton');
+INSERT INTO CATEGORIES (CATEGORY_TITLE, CATEGORY_SUB, CATEGORY_DESCRIPTION)
+VALUES ('Kids & Baby','BABY GIRLS ROMPERS', 'BABY GIRLS ROMPERS, aymarket check romper from BURBERRY KIDS featuring Haymarket check pattern, band collar, long sleeves, 
+front button placket & concealed press-stud fastening, 100% Cotton');
 
--- INSERT INTO CATEGORIES ( CATEGORY_TITLE, CATEGORY_DESCRIPTION)
--- VALUES ('Home & Kitchen', 'The Barista Express Coffee Machine, aH40cm x W33cm x D31cm approx.UK plugCapacity: 2L approx. water tank with integrated filtration; 
--- 480ml approx. stainless steel milk jug');
+INSERT INTO CATEGORIES (CATEGORY_TITLE, CATEGORY_SUB, CATEGORY_DESCRIPTION)
+VALUES ('Home & Kitchen', 'Coffee Machine','The Barista Express Coffee Machine, aH40cm x W33cm x D31cm approx.UK plugCapacity: 2L approx. water tank with integrated filtration; 
+480ml approx. stainless steel milk jug');
 
--- INSERT INTO CATEGORIES ( CATEGORY_TITLE, CATEGORY_DESCRIPTION)
--- VALUES (' Kitchen', 'Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
--- The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
--- The 100% combed cotton rich fibres create extra softness and absorbency., 100% Cotton');
+INSERT INTO CATEGORIES (CATEGORY_TITLE, CATEGORY_SUB, CATEGORY_DESCRIPTION)
+VALUES ('Home & Kitchen','Bathroom decor', 'Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
+The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
+The 100% combed cotton rich fibres create extra softness and absorbency., 100% Cotton');
 
--- INSERT INTO CATEGORIES ( CATEGORY_TITLE, CATEGORY_DESCRIPTION)
--- VALUES (' MENS SHIRT', 'Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
--- The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
--- The 100% combed cotton rich fibres create extra softness and absorbency., 100% Cotton');
+INSERT INTO CATEGORIES (CATEGORY_TITLE, CATEGORY_SUB, CATEGORY_DESCRIPTION)
+VALUES ('MENS SHIRT', 'Shirt','Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
+The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
+The 100% combed cotton rich fibres create extra softness and absorbency., 100% Cotton');
 
--- INSERT INTO CATEGORIES ( CATEGORY_TITLE, CATEGORY_DESCRIPTION)
--- VALUES (' WOMENS TOPS', 'Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
--- The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
--- The 100% combed cotton rich fibres create extra softness and absorbency., 100% Cotton');
+INSERT INTO CATEGORIES (CATEGORY_TITLE, CATEGORY_SUB, CATEGORY_DESCRIPTION)
+VALUES ('WOMENS TOPS', 'TOPS','Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
+The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
+The 100% combed cotton rich fibres create extra softness and absorbency., 100% Cotton');
 
--- INSERT INTO CATEGORIES ( CATEGORY_TITLE, CATEGORY_DESCRIPTION)
--- VALUES (' BOY HAT', 'Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
--- The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
--- The 100% combed cotton rich fibres create extra softness and absorbency., 100% Cotton');
+INSERT INTO CATEGORIES (CATEGORY_TITLE, CATEGORY_SUB, CATEGORY_DESCRIPTION)
+VALUES ('Kids & Baby','BOY HAT', 'Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
+The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
+The 100% combed cotton rich fibres create extra softness and absorbency., 100% Cotton');
 
--- INSERT INTO CATEGORIES ( CATEGORY_TITLE, CATEGORY_DESCRIPTION)
--- VALUES (' GIRL SCARF', 'Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
--- The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
--- The 100% combed cotton rich fibres create extra softness and absorbency., 100% Cotton');
+INSERT INTO CATEGORIES (CATEGORY_TITLE, CATEGORY_SUB, CATEGORY_DESCRIPTION)
+VALUES ('Kids & Baby','GIRL Top', 'Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
+The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
+The 100% combed cotton rich fibres create extra softness and absorbency., 100% Cotton');
 
 
 
@@ -61,80 +61,80 @@ Filling: 80% Down, 20% Feather
 Trims: 100% Coyote Fur',
  'bc7d650b-f778-4b8d-8f75-1bc1cd35a0b1');
 
--- INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
--- VALUES (2, 'Dresses', 'DOLCE & GABBANA', '38IT', 'Mix Uva/fungh/', 1, 
--- 'WOMENS DESIGNER DRESSES, No stranger to eccentricity, Dolce & Gabbana has rendered this midi dress in the Autumn print, 
---  which sees an amalgamation of patterns come together for a maximalist finish. Crafted in Italy from sumptuous silk satin,
---  this design is signature to the label and exudes luxury courtesy of the pussybow neckline and fluid fit.',
---  '55281671-4357-44bf-aa94-383d35273e05');
+INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
+ VALUES (2, 'Dresses', 'DOLCE & GABBANA', '38IT', 'Mix Uva/fungh/', 1, 
+'WOMENS DESIGNER DRESSES, No stranger to eccentricity, Dolce & Gabbana has rendered this midi dress in the Autumn print, 
+ which sees an amalgamation of patterns come together for a maximalist finish. Crafted in Italy from sumptuous silk satin,
+ this design is signature to the label and exudes luxury courtesy of the pussybow neckline and fluid fit.',
+'55281671-4357-44bf-aa94-383d35273e05');
 
--- INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
--- VALUES (3, 'BOYS T-SHIRTS', 'YOUNG VERSACE', 'M', ' Nero', 2, 
--- 'He may have noticed the vintage-inspired designs taking over the world of streetwear as of late – or maybe not at all if he’s yet to discover his fashion 
---  preferences – but either way, this Young Versace T-shirt is destined to become a staple in his wardrobe. While the craftsmanship is all about comfort with a 
---  profile imagined in soft cotton, the design is very much about an effortlessly cool, distressed look with a faded Medusa logo across the chest..',
---  '05d42b6b-1c2a-4cb1-8ab3-9c39c2eda624');
+ INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
+ VALUES (3, 'BOYS T-SHIRTS', 'YOUNG VERSACE', 'M', ' Nero', 2, 
+ 'He may have noticed the vintage-inspired designs taking over the world of streetwear as of late – or maybe not at all if he’s yet to discover his fashion 
+  preferences – but either way, this Young Versace T-shirt is destined to become a staple in his wardrobe. While the craftsmanship is all about comfort with a 
+  profile imagined in soft cotton, the design is very much about an effortlessly cool, distressed look with a faded Medusa logo across the chest..',
+  '05d42b6b-1c2a-4cb1-8ab3-9c39c2eda624');
 
--- INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
--- VALUES (4, 'BABY GIRLS Haymarket check romper', 'BURBERRY KIDS', '6MTHS', 'Archive Beige Ip Chk', 1, 
--- 'Beige/white/black/red cotton Haymarket check romper from BURBERRY KIDS featuring Haymarket check pattern, band collar, long sleeves, 
---  front button placket and concealed press-stud fastening.',
---  '6eb8d18c-482f-4c74-abbf-e9ce6cbf80ba');
-
-
-
--- INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
--- VALUES (5, 'The Barista Express Coffee Machine', 'SAGE', 'H40cm x W33cm x D31cm approx', 'Metallic', 1, 
--- 'Heston Blumenthal brings professional-grade coffee to the home kitchen with the Barista Express Coffee Machine by Sage. 
--- Extracting the full flavour of whole coffee beans by grinding them directly into the filter, the process, from bean to coffee, 
--- takes less than a minute. Equipped with a Thermacoil heating system and powerful 15 bar pressure system, 
--- the machine offers espresso in both single and double shots, and the option to make two cups of coffee at once, 
--- while a high power milk frothing arm means you can enjoy a latte, cappuccino or flat white in the comfort of your own home.
--- Well-appointed with a warming tray and self-cleaning feature, everything has been thought of, including the sleek,
--- ergonomic design that will look stylish atop your kitchen counter..',
--- 'fa0ba997-3236-43f5-a222-e660c3f8dd9d');
+ INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
+ VALUES (4, 'BABY GIRLS Haymarket check romper', 'BURBERRY KIDS', '6MTHS', 'Archive Beige Ip Chk', 1, 
+ 'Beige/white/black/red cotton Haymarket check romper from BURBERRY KIDS featuring Haymarket check pattern, band collar, long sleeves, 
+ front button placket and concealed press-stud fastening.',
+ '6eb8d18c-482f-4c74-abbf-e9ce6cbf80ba');
 
 
 
---  INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
--- VALUES (6, 'BATH MATS', 'ABYSS & HABIDECOR', '70cm x 120cm', 'Li', 1, 
--- 'Made from pure combed cotton, this bold ombre mat is a soft and contemporary addition to your bedroom.
--- Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
--- The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
--- The 100% combed cotton rich fibres create extra softness and absorbency, Machine wash',
---  '1b7757f2-c6b9-42b9-804d-2a964af3d057 ');
+INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
+VALUES (5, 'The Barista Express Coffee Machine', 'SAGE', 'H40cm x W33cm x D31cm approx', 'Metallic', 1, 
+'Heston Blumenthal brings professional-grade coffee to the home kitchen with the Barista Express Coffee Machine by Sage. 
+Extracting the full flavour of whole coffee beans by grinding them directly into the filter, the process, from bean to coffee, 
+takes less than a minute. Equipped with a Thermacoil heating system and powerful 15 bar pressure system, 
+the machine offers espresso in both single and double shots, and the option to make two cups of coffee at once, 
+while a high power milk frothing arm means you can enjoy a latte, cappuccino or flat white in the comfort of your own home.
+Well-appointed with a warming tray and self-cleaning feature, everything has been thought of, including the sleek,
+ergonomic design that will look stylish atop your kitchen counter..',
+'fa0ba997-3236-43f5-a222-e660c3f8dd9d');
+
+
+
+INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
+VALUES (6, 'BATH MATS', 'ABYSS & HABIDECOR', '70cm x 120cm', 'Li', 1, 
+'Made from pure combed cotton, this bold ombre mat is a soft and contemporary addition to your bedroom.
+Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
+The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
+The 100% combed cotton rich fibres create extra softness and absorbency, Machine wash',
+'1b7757f2-c6b9-42b9-804d-2a964af3d057 ');
  
---   INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
--- VALUES (7, 'MENS SHIRT', 'ABYSS & HABIDECOR', '70cm x 120cm', 'Li', 1, 
--- 'Made from pure combed cotton, this bold ombre mat is a soft and contemporary addition to your bedroom.
--- Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
--- The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
--- The 100% combed cotton rich fibres create extra softness and absorbency, Machine wash',
---  '1b7757f2-c6b9-42b9-804d-2a964af3d057 ');
+INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
+VALUES (7, 'MENS SHIRT', 'ABYSS & HABIDECOR', '70cm x 120cm', 'Li', 1, 
+'Made from pure combed cotton, this bold ombre mat is a soft and contemporary addition to your bedroom.
+Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
+The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
+The 100% combed cotton rich fibres create extra softness and absorbency, Machine wash',
+'1b7757f2-c6b9-42b9-804d-2a964af3d057 ');
  
---   INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
--- VALUES (8, 'WOMANS TOP', 'ABYSS & HABIDECOR', '70cm x 120cm', 'Li', 1, 
--- 'Made from pure combed cotton, this bold ombre mat is a soft and contemporary addition to your bedroom.
--- Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
--- The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
--- The 100% combed cotton rich fibres create extra softness and absorbency, Machine wash',
---  '1b7757f2-c6b9-42b9-804d-2a964af3d057 ');
+INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
+VALUES (8, 'WOMANS TOP', 'ABYSS & HABIDECOR', '70cm x 120cm', 'Li', 1, 
+'Made from pure combed cotton, this bold ombre mat is a soft and contemporary addition to your bedroom.
+Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
+The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
+The 100% combed cotton rich fibres create extra softness and absorbency, Machine wash',
+'1b7757f2-c6b9-42b9-804d-2a964af3d057 ');
+
+INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
+VALUES (9, 'BOY HAT', 'ABYSS & HABIDECOR', '70cm x 120cm', 'Li', 1, 
+'Made from pure combed cotton, this bold ombre mat is a soft and contemporary addition to your bedroom.
+Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
+The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
+The 100% combed cotton rich fibres create extra softness and absorbency, Machine wash',
+ '1b7757f2-c6b9-42b9-804d-2a964af3d057 ');
  
---   INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
--- VALUES (9, 'BOY HAT', 'ABYSS & HABIDECOR', '70cm x 120cm', 'Li', 1, 
--- 'Made from pure combed cotton, this bold ombre mat is a soft and contemporary addition to your bedroom.
--- Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
--- The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
--- The 100% combed cotton rich fibres create extra softness and absorbency, Machine wash',
---  '1b7757f2-c6b9-42b9-804d-2a964af3d057 ');
- 
---   INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
--- VALUES (10, 'GIRLS SCARF', 'ABYSS & HABIDECOR', '70cm x 120cm', 'Li', 1, 
--- 'Made from pure combed cotton, this bold ombre mat is a soft and contemporary addition to your bedroom.
--- Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
--- The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
--- The 100% combed cotton rich fibres create extra softness and absorbency, Machine wash',
---  '1b7757f2-c6b9-42b9-804d-2a964af3d057 ');
+ INSERT INTO PRODUCTS ( CATEGORY_ID, NAME, BRAND, SIZE, COLOUR, QUANTITY, PRO_DESCRIPTION, UUID)
+VALUES (10, 'GIRLS SCARF', 'ABYSS & HABIDECOR', '70cm x 120cm', 'Li', 1, 
+'Made from pure combed cotton, this bold ombre mat is a soft and contemporary addition to your bedroom.
+Freshen up your bathroom decor with this eye-catching ombre ‘Stone’ bath mat from Abyss & Habidecor. 
+The tonal colour palette gives the illusion of a round stone and will add a thoroughly contemporary look to any bathroom. 
+The 100% combed cotton rich fibres create extra softness and absorbency, Machine wash',
+ '1b7757f2-c6b9-42b9-804d-2a964af3d057 ');
 
 
 
